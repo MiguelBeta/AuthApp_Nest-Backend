d9a9ead43b57ce3a8b350e70aa156ef1071ab9cc
Miguel <workingbeta01@gmail.com>
2024-02-22 14:38:03 -0500

    Cors habilitado para manejar un solo dominio con el frontend

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  //Se habilita el metodo para poder manejar un solo dominio entre angular y nest (backend)
+  app.enableCors();
+
   //Esta funcion hace referencia a que la informacion debe venir como se
   //la espera de lo contrario la rechaza
   app.useGlobalPipes(
